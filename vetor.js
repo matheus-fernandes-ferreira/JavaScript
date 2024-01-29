5b376b8e2dd0dd64194748f4b4d70c5fa51d34d3
MATHEUS FERNANDES FERREIRA <a94997052@rn.senac.local>
2024-01-29 16:20:26 -0300

    aula vetores

---
+++ vetor.js
@@ -0,0 +1,21 @@
+var letras=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'];
+var vetor=['azul', 'amarelo', 'preto', 'branco', 'roxo', 'laranja', 'rosa', 'lilas', 'roxo', 'marron', 10, 20, 30, 40, 50, 60, 70, 80, 90, 10]
+
+//for (var cont=0; cont<vetor.length;cont++) {
+    //console.log(vetor[cont])
+//}
+tamanho=vetor.length;
+console.log(tamanho)
+
+for (var cont=((vetor.length)-1); cont>0;cont-=1) {
+    console.log(vetor[cont])
+}
+
+
+
+
+//var index=0;
+//while (index<letras.length) {
+   // console.log(letras[index])
+   // index+=1
+//}
